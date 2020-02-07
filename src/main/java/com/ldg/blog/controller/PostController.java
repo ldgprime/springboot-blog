1721d52bd81390a4918144d1a3d053db0046df1f
ldg <leehearts@naver.com>
2020-02-07 15:02:27 +0900

    시큐리티 구현완료(거짓말)

--- src/main/java/com/ldg/blog/controller/PostController.java
+++ src/main/java/com/ldg/blog/controller/PostController.java
@@ -8,8 +8,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ldg.blog.model.user.User;
+
+//시큐리티 구현완료
 
 
 @Controller
@@ -37,12 +40,15 @@ public class PostController {
 	}
 	
 	//인증 체크, 동일인 인증 null체크 인증체크 하지 않아도 된다.
-	@GetMapping("/post/update/{id}")
-	public String update(@PathVariable int id) {
+	@GetMapping("/post/update/{postId}")
+	public String update(@PathVariable int id, @RequestParam int userId) {
+		
 		
 		User principal = (User) session.getAttribute("principal");
-		if(principal.getId()== id) {
+		
+		if(principal.getId() != userId) {
 			
+			return "/user/login";
 		}
 		
 		return "/post/update";
